cd4c2bdff685301519bbc1b7f890790c37bc780d
Shresth Jain <shresth.jn19@gmail.com>
2025-05-05 12:20:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import pandas as pd
 
-API_URL = "https://shl-recommendation-system-yoow.onrender.com/recommend"  
+API_URL = "https://shl-recommendation-system-yoow.onrender.com"  
 
 st.set_page_config(page_title="SHL Assessment Recommendation", page_icon="🎯", layout="wide")
 
