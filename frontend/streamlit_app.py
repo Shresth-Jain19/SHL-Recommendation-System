a8d99fd1e258acf4e7f12ca2a905a823ab82c8cd
Shresth Jain <shresth.jn19@gmail.com>
2025-05-08 01:49:47 +0530

    Updated URL

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 st.set_page_config(page_title="SHL Assessment Recommendation", page_icon="🎯", layout="wide")
 
-API_URL = "https://shl-recommendation-system-yoow.onrender.com"
+API_URL = "http://13.233.216.63"
 
 # Custom CSS for modern look
 st.markdown("""
